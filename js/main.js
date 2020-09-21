c5a61536cf3a33b4474c1b3963f762ed47ad753d
heydaywebsite <heydaywebsite@gmail.com>
2020-09-21 22:54:32 +0300

    added picture to slider

--- js/main.js
+++ js/main.js
@@ -6,4 +6,8 @@ var mySwiper = new Swiper('.swiper-container', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 })
