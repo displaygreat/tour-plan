28bd46d88c6913402f2a7a369c39398e1af0b13b
heydaywebsite <heydaywebsite@gmail.com>
2020-09-24 15:31:47 +0300

    connected to yandex.maps

---
+++ js/maps.js
@@ -0,0 +1,30 @@
+// function init() {
+//   var myMap = new ymaps.Map("map", {
+//     center: [47.90172153, 1.86241034],
+//     zoom: 15
+//   });
+// }
+
+var myMap;
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init() {
+  // Создание экземпляра карты и его привязка к контейнеру с
+  // заданным id ("map").
+  myMap = new ymaps.Map('map', {
+    // При инициализации карты обязательно нужно указать
+    // её центр и коэффициент масштабирования.
+    center: [42.48929906, 27.47782830], // Бургас
+    zoom: 10
+  }, {
+    searchControlProvider: 'yandex#search'
+  });
+
+  document.getElementById('destroyButton').onclick = function () {
+    // Для уничтожения используется метод destroy.
+    myMap.destroy();
+  };
+
+}
