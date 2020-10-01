a7ce359bca47c1cb6c9905f453d1ef9c67c1df64
heydaywebsite <heydaywebsite@gmail.com>
2020-10-02 01:28:52 +0300

    create: mobile-menu

--- js/main.js
+++ js/main.js
@@ -30,3 +30,10 @@ $('.parallax-window').parallax({
   imageSrc: 'img/newsletter-bg.jpg',
   speed: 0.2,
 });
+
+var menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', function () {
+  console.log('Клик по кнопке меню');
+  document.querySelector('.navbar-bottom')
+    .classList.toggle('navbar-bottom_visible');
+});
