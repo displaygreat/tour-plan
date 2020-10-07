bf301ed192a8c13f7e75aa0bd14a7cd6b20f70aa
heydaywebsite <heydaywebsite@gmail.com>
2020-10-07 16:04:31 +0300

    form validate

--- js/main.js
+++ js/main.js
@@ -63,4 +63,26 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay-visible');
     modalDialog.removeClass('modal__dialog-visible')
   }
+
+  //Обработка форм
+  $('.form').each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minLength: "Еhe name must be at least two letters",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "Phone number is required",
+        },
+
+      },
+    });
+  });
+
 });
